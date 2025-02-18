7892006f132c6a2191499665ac6031edd810d4d7
Juliana Morikoshi <julianamorikoshi@gmail.com>
2025-02-18 19:39:56 -0300

    feat: landing page ui

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,13 +1,8 @@
-import path from 'node:path'
-
 const lintStagedConfigs = {
-  '**.{js|ts|cjs|mjs|d.cts|d.mts|jsx|tsx|json|jsonc}': [
-    'biome check --files-ignore-unknown=true', // Format and check for errors
-    'biome check --write --no-errors-on-unmatched', // Format, organize imports, check for errors, and apply safe fixes
-    'biome check --write --organize-imports-enabled=false --no-errors-on-unmatched', // Format and apply safe fixes
-    'biome check --write --unsafe --no-errors-on-unmatched', // Format, organize imports, check for errors, and apply safe and unsafe fixes
-    'biome format --write --no-errors-on-unmatched', // Formatting
-    'biome lint --write --no-errors-on-unmatched', // Check for errors and apply safe fixes
+  '**.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+    'biome check --files-ignore-unknown=true --write --unsafe --organize-imports-enabled=true --no-errors-on-unmatched',
+    'biome format --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
+    'biome lint --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
     'git add .'
   ]
 }
