b8f5cd9c1ddb4cb8d62b19406d16c409f8043bfd
Juliana Morikoshi <julianamorikoshi@gmail.com>
2025-02-19 01:14:48 -0300

    feat: invite page ui

--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -1,9 +1,13 @@
 import type { ComponentProps } from 'react'
+import { twMerge } from 'tailwind-merge'
 
-export function IconButton(props: ComponentProps<'button'>) {
+export function IconButton({ className, ...props }: ComponentProps<'button'>) {
   return (
     <button
-      className="text-blue hover:bg-blue cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900"
+      className={twMerge(
+        'text-blue hover:bg-blue cursor-pointer rounded-md bg-gray-500 p-1.5 transition-colors duration-300 hover:text-gray-900',
+        className
+      )}
       {...props}
     />
   )
