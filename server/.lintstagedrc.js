2552ec4d38d183ac77508014208616ad2037d53f
Juliana Morikoshi <julianamorikoshi@gmail.com>
2025-02-20 01:22:28 -0300

    feat: initial server config

---
+++ server/.lintstagedrc.js
@@ -0,0 +1,10 @@
+const lintStagedConfigs = {
+  './**.{js,ts,cjs,mjs,d.cts,d.mts,json,jsonc}': [
+    'biome check --files-ignore-unknown=true --write --unsafe --organize-imports-enabled=true --no-errors-on-unmatched',
+    'biome format --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
+    'biome lint --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
+    'git add .'
+  ]
+}
+
+export default lintStagedConfigs
