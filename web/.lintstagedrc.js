410a02dfcb9f5a7717dc07a923a07ff150282cac
Juliana Morikoshi <julianamorikoshi@gmail.com>
2025-02-19 23:56:53 -0300

    chore: web as repo module

--- web/.lintstagedrc.js
+++ web/.lintstagedrc.js
@@ -1,5 +1,5 @@
 const lintStagedConfigs = {
-  '**.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
+  './**.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}': [
     'biome check --files-ignore-unknown=true --write --unsafe --organize-imports-enabled=true --no-errors-on-unmatched',
     'biome format --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
     'biome lint --files-ignore-unknown=true --write --no-errors-on-unmatched --staged',
